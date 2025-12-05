c97bf113615e29179183cde53f2074d91b09cd4d
ramesh <rameshma022000@gmail.com>
2025-12-05 21:26:55 +0200

    todo project completed

--- todo-list/js/todo-list.js
+++ todo-list/js/todo-list.js
@@ -1,15 +1,25 @@
 let todoList = [];
 
-
 renderTodos();
 
-
 function addTodo(){
-  const inputElement = document.querySelector(".js-todo-input");
+  const todoInput = document.querySelector(".js-todo-input");
+  const dateInput = document.querySelector(".js-date-input");
+  const name = todoInput.value;
+  const date = dateInput.value;
 
-  todoList.push(inputElement.value);
+  // todoList.push({
+  //   name: todoInput.value,
+  //   date: dateInput.value
+  // });
+  // ^ shorthand property
+  todoList.push({
+    name,
+    date
+  });
 
-  inputElement.value = '';
+  todoInput.value = '';
+  dateInput.value = '';
 
   renderTodos();
 }
@@ -19,7 +29,20 @@ function renderTodos(){
 
   for(i = 0; i < todoList.length; i++){
     const todo = todoList[i];
-    allTodos += `<p>${todo}</p>`;
+
+    // const todoName = todo.name;
+    // const todoDate = todo.date;
+    // destructuring
+    const {name, date} = todo;
+
+    allTodos += `
+      <div>${name}</div> 
+      <div>${date}</div>
+      <button onclick = "
+        todoList.splice(${i}, 1);
+        renderTodos();  
+      " class="delete-button">Delete</button>
+    `;
   }
 
   document.querySelector(".js-all-todos").innerHTML = allTodos;
